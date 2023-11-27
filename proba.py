5a79c4ec9b9d82cf9edc41f950ebc1a5e2ae969c
Art-Nata <A-H-A.2010@yandex.ru>
2023-11-27 17:33:32 +0300

    добавила печать 1

--- proba.py
+++ proba.py
@@ -7,7 +7,8 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
-    my_superfunction()
+    my_superfunction()\
+    print(1)
 
 
 if __name__ == '__main__':
